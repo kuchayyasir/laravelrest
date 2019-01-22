95f0ff36686de55cd2ec5db0e03b561e65d3b060
yasir.kuchay@lelafe.com <yasir.kuchay@lelafe.com>
2019-01-22 10:43:29 +0530

    restful

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Http\Resources\Category as CategoryResource;
+use Illuminate\Http\Request;
+
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Get all categories
+        $categories = Category::get();
+ 
+        // Return a collection of $categories with pagination
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $category =  new Category;
+        $category->name = $request->input('name');
+        //$category->description = $request->input('description');
+        if($category->save()) {
+            return new CategoryResource($category);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //Get the Category
+        $category = Category::findOrfail((int)$category->id);
+        // Return a single Category
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+        dd($category);
+        $category = Category::findOrFail((int)$request->id);
+        $category->name = $request->input('name');
+        //$category->description = $request->input('description');
+        if($category->save()) {
+            return new CategoryResource($category);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+        
+        //Get the Category
+        $category = Category::findOrfail((int)$category->id);
+ 
+        if($category->delete()) {
+            return new CategoryResource($category);
+        } 
+    }
+}
