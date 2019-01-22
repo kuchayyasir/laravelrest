81f4150eab9aacabb50a2c7d55d65cd4dd731a63
yasir.kuchay@lelafe.com <yasir.kuchay@lelafe.com>
2019-01-22 14:05:52 +0530

    jwt

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 100)->create();
         factory(App\Category::class, 100)->create();
     }
 }
